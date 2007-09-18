58f3fdaead7d65f5b7ac1e972db07a3b50d4e6d4
andrew.kirillov <andrew.kirillov@87d9d96e-603a-0410-a9c8-4181b64b1b3e>
2007-09-18 04:25:31 +0000

    2.5.1 release related updates.

--- Sources/AssemblyInfo.cs
+++ Sources/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("AForge")]
 [assembly: AssemblyProduct( "IPLab" )]
-[assembly: AssemblyCopyright( "IPLab © 2007" )]
+[assembly: AssemblyCopyright( "AForge © 2007" )]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
 
@@ -26,7 +26,8 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.5.0.0")]
+[assembly: AssemblyVersion("2.5.1.0")]
+[assembly: AssemblyFileVersion( "2.5.1.0" )]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
