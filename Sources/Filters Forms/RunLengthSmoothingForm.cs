b882f92b1f2549377beec27b9ca449cc1c879f74
andrew.kirillov <andrew.kirillov@87d9d96e-603a-0410-a9c8-4181b64b1b3e>
2012-03-19 19:34:18 +0000

    Add Horizontal/Vertical run length smoothing algorithm to the application.

---
+++ Sources/Filters Forms/RunLengthSmoothingForm.cs
@@ -0,0 +1,79 @@
+﻿// Image Processing Lab
+// http://www.aforgenet.com/projects/iplab/
+//
+// Copyright © Andrew Kirillov, 2005-2012
+// andrew.kirillov@aforgenet.com
+//
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using AForge.Imaging.Filters;
+
+namespace IPLab
+{
+    public partial class RunLengthSmoothingForm : Form
+    {
+        private IFilter filter = null;
+
+        // Image property
+        public Bitmap Image
+        {
+            set { filterPreview.Image = value; }
+        }
+
+        // Filter property
+        public IFilter Filter
+        {
+            get { return filter; }
+        }
+
+        // Constructor
+        public RunLengthSmoothingForm( )
+        {
+            InitializeComponent( );
+
+            typeCombo.SelectedIndex = 0;
+            maxGapUpDown.Value = 10;
+        }
+
+        private void typeCombo_SelectedIndexChanged( object sender, EventArgs e )
+        {
+            int maxGapSize = (int) maxGapUpDown.Value;
+
+            if ( typeCombo.SelectedIndex == 0 )
+            {
+                filter = new HorizontalRunLengthSmoothing( maxGapSize );
+            }
+            else
+            {
+                filter = new VerticalRunLengthSmoothing( maxGapSize );
+            }
+
+            filterPreview.Filter = filter;
+            filterPreview.RefreshFilter( );
+        }
+
+        private void maxGapUpDown_ValueChanged( object sender, EventArgs e )
+        {
+            int maxGapSize = (int) maxGapUpDown.Value;
+
+            if ( typeCombo.SelectedIndex == 0 )
+            {
+                ( (HorizontalRunLengthSmoothing) filter ).MaxGapSize = maxGapSize;
+            }
+            else
+            {
+                ( (VerticalRunLengthSmoothing) filter ).MaxGapSize = maxGapSize;
+            }
+
+            filterPreview.Filter = filter;
+            filterPreview.RefreshFilter( );
+        }
+    }
+}
