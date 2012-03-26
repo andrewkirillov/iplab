178cbe97819c37dfc60730dec81675ddfdd678d8
andrew.kirillov <andrew.kirillov@87d9d96e-603a-0410-a9c8-4181b64b1b3e>
2012-03-26 20:15:27 +0000

    Added configuration/preview form for Move Towards filter.

---
+++ trunk/Sources/Filters Forms/MoveTowardsFilterForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace IPLab
+{
+    partial class MoveTowardsFilterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose( );
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent( )
+        {
+            this.stepSizeBar = new System.Windows.Forms.TrackBar( );
+            this.label1 = new System.Windows.Forms.Label( );
+            this.groupBox1 = new System.Windows.Forms.GroupBox( );
+            this.filterPreview = new IPLab.FilterPreview( );
+            this.pictureBox1 = new System.Windows.Forms.PictureBox( );
+            this.cancelButton = new System.Windows.Forms.Button( );
+            this.okButton = new System.Windows.Forms.Button( );
+            ( (System.ComponentModel.ISupportInitialize) ( this.stepSizeBar ) ).BeginInit( );
+            this.groupBox1.SuspendLayout( );
+            ( (System.ComponentModel.ISupportInitialize) ( this.pictureBox1 ) ).BeginInit( );
+            this.SuspendLayout( );
+            // 
+            // stepSizeBar
+            // 
+            this.stepSizeBar.Location = new System.Drawing.Point( 8, 35 );
+            this.stepSizeBar.Maximum = 255;
+            this.stepSizeBar.Minimum = 1;
+            this.stepSizeBar.Name = "stepSizeBar";
+            this.stepSizeBar.Size = new System.Drawing.Size( 220, 45 );
+            this.stepSizeBar.TabIndex = 1;
+            this.stepSizeBar.TickFrequency = 5;
+            this.stepSizeBar.Value = 10;
+            this.stepSizeBar.Scroll += new System.EventHandler( this.stepSizeBar_Scroll );
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point( 10, 10 );
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size( 110, 15 );
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Step size:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add( this.filterPreview );
+            this.groupBox1.Location = new System.Drawing.Point( 230, 10 );
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size( 170, 175 );
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Preview";
+            // 
+            // filterPreview
+            // 
+            this.filterPreview.Image = null;
+            this.filterPreview.Location = new System.Drawing.Point( 10, 15 );
+            this.filterPreview.Name = "filterPreview";
+            this.filterPreview.Size = new System.Drawing.Size( 150, 150 );
+            this.filterPreview.TabIndex = 0;
+            this.filterPreview.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point( 10, 182 );
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size( 210, 2 );
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cancelButton.Location = new System.Drawing.Point( 209, 195 );
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size( 75, 23 );
+            this.cancelButton.TabIndex = 4;
+            this.cancelButton.Text = "Cancel";
+            // 
+            // okButton
+            // 
+            this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.okButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.okButton.Location = new System.Drawing.Point( 124, 195 );
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size( 75, 23 );
+            this.okButton.TabIndex = 3;
+            this.okButton.Text = "OK";
+            // 
+            // MoveTowardsFilterForm
+            // 
+            this.AcceptButton = this.okButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF( 6F, 13F );
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancelButton;
+            this.ClientSize = new System.Drawing.Size( 409, 226 );
+            this.Controls.Add( this.pictureBox1 );
+            this.Controls.Add( this.cancelButton );
+            this.Controls.Add( this.okButton );
+            this.Controls.Add( this.groupBox1 );
+            this.Controls.Add( this.stepSizeBar );
+            this.Controls.Add( this.label1 );
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MoveTowardsFilterForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Move Towards";
+            ( (System.ComponentModel.ISupportInitialize) ( this.stepSizeBar ) ).EndInit( );
+            this.groupBox1.ResumeLayout( false );
+            ( (System.ComponentModel.ISupportInitialize) ( this.pictureBox1 ) ).EndInit( );
+            this.ResumeLayout( false );
+            this.PerformLayout( );
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar stepSizeBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private FilterPreview filterPreview;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button okButton;
+    }
+}
---
+++ trunk/Sources/Filters Forms/MoveTowardsFilterForm.cs
@@ -0,0 +1,54 @@
+﻿// Image Processing Lab
+// http://www.aforgenet.com/projects/iplab/
+//
+// Copyright © Andrew Kirillov, 2005-2012
+// andrew.kirillov@aforgenet.com
+//
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using AForge.Imaging.Filters;
+
+namespace IPLab
+{
+    public partial class MoveTowardsFilterForm : Form
+    {
+        private MoveTowards filter;
+
+        // Image property
+        public Bitmap Image
+        {
+            set { filterPreview.Image = value; }
+        }
+        // Filter property
+        public IFilter Filter
+        {
+            get { return filter; }
+        }
+
+        // Constructor
+        public MoveTowardsFilterForm( Bitmap overlay )
+        {
+            InitializeComponent( );
+
+            // create filter
+            filter = new MoveTowards( overlay );
+            // set percent value on track bar
+            stepSizeBar.Value = (int) ( filter.StepSize );
+            // set filter for preview window
+            filterPreview.Filter = filter;
+        }
+
+        private void stepSizeBar_Scroll( object sender, EventArgs e )
+        {
+            filter.StepSize = (int) stepSizeBar.Value;
+            filterPreview.RefreshFilter( );
+        }
+    }
+}
